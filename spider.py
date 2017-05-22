a1b9c2b3095d416edbb5528eb50d174c39799746
Misaki526 <misaki_526@163.com>
2017-05-22 21:17:20 +0800

    version_2

--- spider.py
+++ spider.py
@@ -11,40 +11,66 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8') # set encoding
 
+# 以 XML 格式输出
 def catagory(html):
-    # 以追加的方式打开
-    fout = open("data.txt", "a")
+    fout = open("data2.txt", "a")
+
+    # 每个疾病的XML开头标签
+    fout.write("<disease>\n")
 
     soup = BeautifulSoup(html)
 
-    # 疾病名
+    # -------疾病名 start----------------------------------
     name = soup.h1.string
-    fout.write(name + "\n")
+    fout.write("<name>" + name + "</name>\n")
+    # -------疾病名 end------------------------------------
 
-    # 基本信息
+
+    # -------基本信息，作为疾病的属性 start------------------
     baseNameList = soup.select('.basic-info dt')
     baseValueList = soup.select('.basic-info dd')
-    for i in range(0, len(baseNameList)):
-        fout.write(baseNameList[i].get_text() + "\t" + baseValueList[i].get_text().replace("\n", "") + "\n")   # 去掉value中自带的回车
 
-    # 各个属性
+    fout.write("<properties>\n")
+    for i in range(0, len(baseNameList)):
+        fout.write("<" + baseNameList[i].get_text() + ">")
+        fout.write(baseValueList[i].get_text().replace("\n", ""))   # 去掉value中自带的回车
+        fout.write("</" + baseNameList[i].get_text() + ">\n")
+    fout.write("</properties>\n")
+    # -------基本信息，作为疾病的属性 end-------------------
+
+
+    # ------具体的详细内容则按照新的实体处理 start----------
     nodeList = soup.select('.title-text span')
     for node in nodeList:
-        # 名称
-        fout.write(node.nextSibling + "\t")
-        value = node.parent.parent.next_sibling.next_sibling
-        TYPEOFTAG = value     #用于后面判断类型，暂时没想到更好的办法
+        # 名称，如病因，治疗等等（开始标签）
+        fout.write("<" + node.nextSibling + ">\n")
 
+        value = node.parent.parent.next_sibling.next_sibling
+        TYPEOFTAG = value     # 用于后面判断类型，暂时没想到更好的办法
+
+
+        # 具体遍历每一个小标题
+        # FILO，以后可以考虑用stack来存放，在合适的地方输出结束标签（一） > 1 > （1）
         while (1):
             if (type(value) == type(TYPEOFTAG)):
                 if (not value.has_attr('class') or value['class'][0] != 'para'):
                     break
-                if len(value.select('b')) > 0 and (value.select('b')[0].get_text()[1:2] == '.' or value.select('b')[0].get_text()[1:2] == '．'):           # 暂时只找形如'1.'类型的值
-                    fout.write(value.select('b')[0].get_text()[2:] + "\t")
+
+                if len(value.select('b')) > 0:
+                    if value.select('b')[0].get_text()[1:2] == "." or value.select('b')[0].get_text()[1:2] == "．":
+                        fout.write("<" + value.select('b')[0].get_text()[2:] + ">")
+                        fout.write(value.next_sibling.next_sibling.get_text())
+                        fout.write("</" + value.select('b')[0].get_text()[2:] + ">\n")
 
             value = value.next_sibling
 
-        fout.write("\n")
+        # 名称（结束标签）
+        fout.write("</" + node.nextSibling + ">\n")
+    # ------具体的详细内容则按照新的实体处理 end ------------
+
+
+    # 每个疾病的XML结束标签
+    fout.write("</disease>\n")
 
     fout.flush()
     fout.close()
